d546ee49f990e2c90aa86431684c5e1acbf5487f
agustin cordero <agustin.cordero201@gmail.com>
2023-06-17 16:43:04 -0300

    second commit

--- multiplier.ts
+++ multiplier.ts
@@ -1,5 +1,25 @@
-const multiplacator = (a, b, printText) =>{
+const multiplacator = (a: number, b:number, printText:string) =>{
     console.log(printText, a * b )
 }
 
-multiplacator(2, 4, 'Multipliet numbers 2 and 4, the result is: ')
+const parseArguments =(args) =>{
+    if (args.length !== 4) throw new Error('Wrong number of arguments')
+
+    const firstNumber =Number(args[2])
+    const secondNumber =Number(args[3])
+
+    if (!isNaN(firstNumber) && !isNaN(secondNumber)){
+      return[
+        firstNumber,
+        secondNumber
+      ]
+    }
+        throw Error('Provided values were not numbers!')
+}
+
+const cleanArguments =parseArguments(process.argv)
+
+const a: number =Number(process.argv[2])
+const b: number =Number(process.argv[3])
+
+multiplacator(a,b,`Multiplied ${a} and ${b} and the result is:`)
